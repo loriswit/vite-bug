b2308168da61eb08c3c6c5e77dfea744e918420d
Loris Witschard <loris.wit@gmail.com>
2020-12-01 16:48:03 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>VITE_VAR: {{ viteVar }}</p>
+  <p>VITE_EXPOSED: {{ viteExposed }}</p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    viteVar: import.meta.env.VITE_VAR,
+    viteExposed: import.meta.env.VITE_EXPOSED
+  })
+}
+</script>
